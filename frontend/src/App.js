6ac524d1c169b962ec2eeddb94f78668aaa7e048
VidyaSagarMehar <vidyasagark890@gmail.com>
2022-12-02 20:57:22 +0530

    BASE URL added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import axios from 'axios';
 import { Form } from './components/Form';
 import { UserList } from './components/UserList';
 import './index.css';
+
+const BASE_URL = 'https://dbquerycrud-app-using-mern-production.up.railway.app';
 
 function App() {
 	return (
